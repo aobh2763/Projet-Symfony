3efad1e7e6d2cd2a889b1428defe50d8b06e7bab
HoussemChourou <houssam.chourou@insat.usar.tn>
2025-05-15 17:35:49 +0100

    html to twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,7 +11,7 @@ final class MainController extends AbstractController
     #[Route('/main', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('main/index.html', [
+        return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
