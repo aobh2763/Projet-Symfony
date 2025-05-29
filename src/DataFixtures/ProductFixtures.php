5e891bb0d22a1ae9c0f8ae50f322374b04408f1f
khlkarim <karimkahili002@gmail.com>
2025-05-29 18:58:47 +0100

    removed unnecessary files

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -9,7 +9,7 @@ use App\Entity\Melee;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class ProductFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
