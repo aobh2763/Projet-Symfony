7f63c45ed77f1d6d1929b38eee321b6023c70269
khlkarim <karimkahili002@gmail.com>
2025-05-29 19:27:52 +0100

    added user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,48 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 
-class UserFixtures extends Fixture
+use App\Entity\{User, Cart, Wishlist};
+
+class UserFixtures extends Fixture implements FixtureGroupInterface
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 5; $i++) {
+            $user = new User();
+
+            $user->setUsername("user$i")
+                ->setEmail("user$i@example.com")
+                ->setFirstName("FirstName$i")
+                ->setLastName("LastName$i")
+                ->setIsVerified(true)
+                ->setPassword($this->passwordHasher->hashPassword($user, 'password123'));
+
+            $cart = new Cart();
+            $user->setCart($cart);
+
+            $wishlist = new Wishlist();
+            $user->setWishlist($wishlist);
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['users'];
     }
 }
