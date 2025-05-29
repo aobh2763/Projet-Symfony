6a343d8181a3ae7e45e5d5d0b5afb69967fd5008
khlkarim <karimkahili002@gmail.com>
2025-05-29 11:01:45 +0100

    added routes for updating and deleting products for the admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,15 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\{Response, RequestStack};
 
-use App\Service\AdminService;
+use App\Service\{AdminService, MainService};
 use App\Entity\Product;
-use App\Form\OrdersFilterTypeForm;
+use App\Form\{OrdersFilterTypeForm, FilterTypeForm};
 
 final class AdminController extends AbstractController
 {
     public function __construct(
         private RequestStack $requestStack,
-        private AdminService $adminService
+        private AdminService $adminService,
+        private MainService $mainService
     ){}
 
     #[Route('/admin', name: 'app_admin')]
@@ -74,4 +75,53 @@ final class AdminController extends AbstractController
             'form' => $form
         ]);
     }   
+
+    #[Route('/admin/products', name: 'app_admin_products')]
+    public function getProducts(){
+        $request = $this->requestStack->getCurrentRequest();
+
+        $form = $this->createForm(FilterTypeForm::class, null, [
+            'method' => 'GET'
+        ]);
+        $form->handleRequest($request);
+
+        $filters = $form->isSubmitted() && $form->isValid()
+            ? $form->getData()
+            : [];
+
+        $page = $request->query->getInt('page', 1);
+        $limit = $request->query->getInt('limit', 10);
+
+        $products = $this->mainService->getProducts($filters, $page, $limit);
+
+        return $this->render('admin/products.html.twig', [
+            'products' => $products,
+            'filterform' => $form,
+            'filters' => $filters,
+        ]);
+    }
+
+    #[Route('/admin/product-details/{id}', name: 'app_admin_product_details')]
+    public function getProductDetails(int $id){
+        $request = $this->requestStack->getCurrentRequest();
+
+        $product = $this->mainService->getProduct($id);
+        $form = $this->createForm(CreateProductTypeForm::class, $product);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $this->adminService->updateProduct($product);
+        }
+
+        return $this->render('admin/product-details.html.twig',[
+            'product' => $product,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/admin/product-details/delete/{id}', 'app_admin_delete_product')]
+    public function deleteProduct(int $id){
+        $this->adminService->deleteProduct($id);
+        return $this->redirectToRoute('app_admin_products');
+    }
 }
